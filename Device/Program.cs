c69d85d1819809c2731bd22db59c2656e017b446
Hugh Maaskant <hugh.maaskant@outlook.com>
2021-07-12 15:34:46 +0200

    Add (empty) Device project to the solution

---
+++ Device/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace IoTAS.Device
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Device");
+        }
+    }
+}
