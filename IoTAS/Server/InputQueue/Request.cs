4f6fa10b40e5a6c6ec8dfecaf04aff8e68c26351
Hugh Maaskant <hugh.maaskant@outlook.com>
2021-07-13 19:50:07 +0200

    Initial definition of request messages

---
+++ IoTAS/Server/InputQueue/Request.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using IoTAS.Shared.Hubs;
+
+namespace IoTAS.Server.InputQueue
+{
+    /// <summary>
+    /// The Request record that gets queued on input from a Hub
+    /// </summary>
+    public record Request(DateTime ReceivedAt, IHubInDTO Data)
+    {
+
+    }
+    
+
+    
+}
