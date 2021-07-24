b54f5b8d6fe2eac364d9d671093b35ed2b481b8c
Hugh Maaskant <hugh.maaskant@outlook.com>
2021-07-24 19:49:24 +0200

    Add DeviceStatus

---
+++ IoTAS/Server/DevicesStatusStore/DeviceStatus.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace IoTAS.Server.DevicesStatusStore
+{
+    public class DeviceStatus
+    {
+        /// <summary>
+        /// The human name for the Device
+        /// </summary>
+        public string DeviceName { get; private set; }
+
+        /// <summary>
+        /// // The very first registration DateTime
+        /// </summary>
+        public DateTime FirstRegisteredAt { get; private set; }
+
+        /// <summary>
+        /// The most recent registration <see cref="DateTime"/>
+        /// </summary>
+        public DateTime LastRegisteredAt { get; private set; }
+
+        /// <summary>
+        /// The most recently seen registration or heartbeat <see cref="DateTime"/>
+        /// </summary>
+        public DateTime LastSeenAt { get; private set; }
+
+    }
+}
